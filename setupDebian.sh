2332714a60d28756d8e713e1b8fe2dc4ba24e83c
beemoon <contact@beemoon.fr>
2015-06-28 22:23:52 +0200

    new script for dev
    first comment dev

--- setupDebian.sh
+++ setupDebian.sh
@@ -14,17 +14,18 @@ clear
 echo Installation de Xorg
 sleep 2
 
-
+#Detection de la carte grapphique
 vgaCard=`lspci -nvv|grep -i "virtualbox graphics"|wc -l`
 
 apt-get -y install --no-install-recommends xinit xserver-xorg x11-xserver-utils xserver-xorg-core xfonts-base xserver-xorg-input-all xserver-xorg-video-fbdev
-#apt-get -y install --no-install-recommends xserver-xorg-video-intel
 # Pour une VM Virtualbox il faut installer les addins afin d'avoir les drivers video.
-if [ $vgaCard==1 ];then 
+if [ $vgaCard==1 ];then
+    vbox=1
     wget https://raw.githubusercontent.com/beemoon/setupDeb/master/VBoxLinuxAdditions.run
     chmod u+x VBoxLinuxAdditions.run
     ./VBoxLinuxAdditions.run
 fi
+#apt-get -y install --no-install-recommends xserver-xorg-video-intel
 clear
 
 echo Installation de Openbox
@@ -32,17 +33,17 @@ sleep 2
 apt-get -y install --no-install-recommends openbox obconf obmenu
 clear
 
-echo Installation du gestinnaire de login
+echo Installation de XTerm
+sleep 2
+apt-get -y install --no-install-recommends xterm
+clear
+
+echo Installation du gestionnaire de login
 sleep 2
 # SLiM est mort !!!
 apt-get -y install --no-install-recommends lightdm
 clear
 exit
-
-echo Installation de deborphan
-sleep 2
-apt-get -y install --no-install-recommends deborphan
-clear
 
 echo Installation de Iceweasel
 sleep 2
@@ -57,11 +58,6 @@ clear
 echo Installation de Thunar
 sleep 2
 apt-get -y install --no-install-recommends thunar thunar-archive-plugin thunar-volman exfat-utils gvfs gvfs-backends
-#apt-get -y install --no-install-recommends thunar-archive-plugin
-#apt-get -y install --no-install-recommends thunar-volman
-#apt-get -y install --no-install-recommends exfat-utils gvfs
-##pour le montage de samba (CIFS)
-#apt-get -y install --no-install-recommends gvfs-backends
 clear
 
 echo Installation de Xarchiver
@@ -74,11 +70,19 @@ sleep 2
 apt-get -y install --no-install-recommends terminator
 clear
 
-echo Installation du firmware iwlwifi
+if [ ! $vbox==1 ];then
+    echo Installation du firmware iwlwifi
+    sleep 2
+    # Debian 8 "Jessie"
+    echo "deb http://http.debian.net/debian/ jessie main contrib non-free" >> /etc/apt/sources.list
+    apt-get -y update
+    apt-get -y install --no-install-recommends firmware-iwlwifi wpasupplicant wicd
+    clear
+fi
+
+echo Installation de deborphan
 sleep 2
-# Debian 8 "Jessie"
-#deb http://http.debian.net/debian/ jessie main contrib non-free
-#apt-get -y install --no-install-recommends firmware-iwlwifi wpasupplicant wicd
+apt-get -y install --no-install-recommends deborphan
 clear
 
 echo Installation de Nitrogen
