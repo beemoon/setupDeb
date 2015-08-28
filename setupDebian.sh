cf92531d752fd0ec17d3a94db27842f7765af1eb
beemoon <contact@beemoon.fr>
2015-08-28 22:14:12 +0200

    nouveau setup

--- setupDebian.sh
+++ setupDebian.sh
@@ -8,6 +8,18 @@ echo
 echo
 }
 
+function install(){
+    package=$*
+    echo -e Installation de $package
+    echo
+    sleep 2
+    apt-get -y install --no-install-recommends $package
+    echo
+    sleep 2
+}
+
+# Mise a jour des depots
+########################
 echo Mise a jour des sources.list
 sleep 2
 echo
@@ -15,27 +27,18 @@ apt-get -y update && apt-get -y upgrade
 sleep 2
 clear
 
-echo Installation de Sudo
-sleep 2
-echo
-apt-get -y install sudo
-sleep 2
-clear
 
-echo Installation de SSH serveur
-sleep 2
-echo
-apt-get -y install openssh-server
-sleep 2
-clear
 
-#Detection de la carte graphique
+# Installation de la carte graphique
+####################################
 vgaCard=`lspci|grep -i "virtualbox graphics"|wc -l`
+
 echo -e Installation du mode graphique
 echo
+sleep 2
 apt-get -y install --no-install-recommends xinit xserver-xorg x11-xserver-utils xserver-xorg-core xfonts-base xserver-xorg-input-all xserver-xorg-video-fbdev
-# Pour une VM Virtualbox il faut installer les addins afin d'avoir les drivers video.
 
+# Pour une VM Virtualbox il faut installer les addins afin d'avoir les drivers video.
 if [ $vgaCard -eq 1 ];then
     vbox=1
     if [ `lsmod | grep vboxguest|wc -l` -eq 0 ];then
@@ -51,104 +54,39 @@ if [ $vgaCard -eq 1 ];then
 else
 	apt-get -y install --no-install-recommends xserver-xorg-video-intel
 fi
+echo
 sleep 2
-clear
 
-echo Installation de Openbox
-sleep 2
-apt-get -y install --no-install-recommends openbox obconf obmenu
-sleep 2
-clear
 
+
+# Installation des paquets de ma distribution « myDeb »
+#######################################################
+wget https://raw.githubusercontent.com/beemoon/setupDeb/dev/myDeb.txt
+while read line  
+do
+    install $line
+done < myDeb.txt
+rm -f myDeb.txt
+
+
+
+# Installation du wifi
+######################
 if [ $vbox -ne 1 ];then
     echo Installation du firmware iwlwifi
+    echo
     sleep 2
     # Debian 8 "Jessie"
     echo "deb http://http.debian.net/debian/ jessie main contrib non-free" >> /etc/apt/sources.list
     apt-get -y update
     apt-get -y install --no-install-recommends firmware-iwlwifi wpasupplicant wicd
+    echo
     sleep 2
-    clear
 fi
 
-echo Installation de Terminator
-sleep 2
-apt-get -y install --no-install-recommends terminator
-sleep 2
-clear
 
-echo Installation du gestionnaire de login
-sleep 2
-# SLiM est mort !!!
-apt-get -y install --no-install-recommends lightdm
-sleep 2
-clear
-
-echo Installation de Nitrogen
-sleep 2
-wget -P /usr/local/images/wallpapers https://raw.githubusercontent.com/beemoon/setupDeb/master/cyborg0.jpg
-apt-get -y install --no-install-recommends nitrogen
-nitrogen --save --set-auto /usr/local/images/wallpapers/cyborg0.jpg
-sleep 2
-clear
-
-echo Installation de Iceweasel
-sleep 2
-apt-get -y install --no-install-recommends iceweasel
-sleep 2
-clear
-
-echo Installation de geany
-sleep 2
-apt-get -y install --no-install-recommends geany
-sleep 2
-clear
-
-echo Installation de Thunar
-sleep 2
-apt-get -y install --no-install-recommends thunar thunar-archive-plugin thunar-volman exfat-utils gvfs gvfs-backends
-sleep 2
-clear
-
-echo Installation de Xarchiver
-sleep 2
-apt-get -y install --no-install-recommends xarchiver
-sleep 2
-clear
-
-echo Installation de deborphan
-sleep 2
-apt-get -y install --no-install-recommends deborphan
-sleep 2
-clear
-
-echo Installation de Tint2
-sleep 2
-apt-get -y install --no-install-recommends tint2
-sleep 2
-clear
-
-echo Installation de Conky
-sleep 2
-apt-get -y install --no-install-recommends conky
-sleep 2
-clear
-
-echo Installation de Icedove
-sleep 2
-apt-get -y install --no-install-recommends icedove
-sleep 2
-clear
-
-echo Installation du son avec Alsa
-sleep 2
-apt-get -y install --no-install-recommends libasound2 alsa-base alsa-utils alsa-oss pavucontrol
-sleep 2
-clear
-
-
-##################################
-# Paquets actuellement installes #
+# Paquets supplementaires a installer
+################################
 dpkg --get-selections >mesPaquets.txt
 sleep 2
 clear
@@ -160,40 +98,35 @@ if [ -e packages.txt ];then
 fi 
 wget https://raw.githubusercontent.com/beemoon/setupDeb/dev/packages.txt
 
-# Difference avec les paquets demandes
+# Difference avec les paquets demandes et ce qui est deja installe
 if [ -e diff.txt ]; then rm -f diff.*; fi
 while read ligne
 do  
-	if [ `grep $ligne mesPaquets.txt|wc -l` -eq 0 ];
-	then
-		echo $ligne>>diff.txt
-		echo $ligne
-	fi
+    if [ `grep $ligne mesPaquets.txt|wc -l` -eq 0 ];
+    then
+            echo $ligne>>diff.txt
+            echo $ligne
+    fi
 
 done < packages.txt
 rm -f mesPaquets.txt
+echo
 sleep 2
-clear
 
 if [ -e diff.txt ];
 then
-
-while read line  
-do
-    echo -e Installation de $line
-    echo
-    sleep 2
-
-    apt-get -y install --no-install-recommends $line
-    echo
-
-done < diff.txt
-rm -f diff.txt
+    while read line  
+    do
+        install $line
+    done < diff.txt
+    rm -f diff.txt
 
 else
-echo Il n\'y a rien a installer
-echo
+    echo Il n\'y a rien a installer
+    echo
 fi
 
 rm -f packages.txt
+
 history -c
+reboot
