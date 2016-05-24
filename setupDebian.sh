fe7b5682fd46950ee8f3cc1b844d4f3f660cda98
Beemoon and Co <contact@beemoon.fr>
2016-05-24 22:18:39 +0200

    ajout du linux header et dkms
    obligatoire pour que les addons de VBox s'installent

--- setupDebian.sh
+++ setupDebian.sh
@@ -42,7 +42,7 @@ if [ $vgaCard -eq 1 ];then
     	if [ -e VBoxLinuxAdditions.run ];then
      		rm -f VBoxLinuxAdditions*
     	fi 
-	apt-get -y install make
+	apt-get -y install make dkms linux-headers-$(uname -r)
     	echo
 	wget https://raw.githubusercontent.com/beemoon/setupDeb/master/VBoxLinuxAdditions.run
     	chmod u+x VBoxLinuxAdditions.run
